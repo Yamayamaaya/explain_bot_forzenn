926514f9b4ab3dc089e05b846c52d2ffc8f02a27
Yamayamaaya <yugo139013@gmail.com>
2023-06-13 01:48:28 +0900

    OpenAiによる語句の説明を導入

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function App() {
-    const [inputSets, setInputSets] = useState([{ term: "", genre: "" }]);
+    const [inputSets, setInputSets] = useState([{}]);
+    const [contents, setContents] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const handleInputChange = (index, event) => {
         const { name, value } = event.target;
@@ -12,7 +14,7 @@ function App() {
     };
 
     const handleAddSet = () => {
-        setInputSets([...inputSets, { term: "", genre: "" }]);
+        setInputSets([...inputSets, { word: "", genre: "" }]);
     };
 
     const handleRemoveSet = (index) => {
@@ -21,12 +23,101 @@ function App() {
         setInputSets(sets);
     };
 
-    const handleSubmit = () => {
-        main();
+    const handleSubmitToOpenAi = async () => {
+        await Promise.all(
+            inputSets.map(async (set) => {
+                await getExplain(set.genre, set.word);
+            })
+        );
+    };
+
+    const handleContentChange = (index, event) => {
+        const { value } = event.target;
+        setContents((prevContents) => {
+            const updatedContents = [...prevContents];
+            updatedContents[index] = {
+                ...updatedContents[index],
+                content: value,
+            };
+            return updatedContents;
+        });
+    };
+
+    const handleCheckChange = (index) => {
+        setContents((prevContents) => {
+            const updatedContents = [...prevContents];
+            updatedContents[index] = {
+                ...updatedContents[index],
+                check: !updatedContents[index].check,
+            };
+            return updatedContents;
+        });
+    };
+
+    const handleSubmitToZenn = async () => {
+        // コンテンツのチェックが入っているものだけをループ処理で足し合わせる。
+        const explains = contents
+            .filter((content) => content.check)
+            .map((content) => content.content)
+            .join("\n\n");
+
+        // Stateの初期化
+        setInputSets([{}]);
+        setContents([]);
+        setLoading(false);
+
+        // Zennへの投稿処理
+        main(explains);
+    };
+
+    const API_URL = "https://api.openai.com/v1/";
+    const MODEL = "gpt-3.5-turbo";
+    const API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
+
+    const getExplain = async (genre, word) => {
+        setLoading(true);
+        try {
+            const prompt = `
+              ${genre}に関する用語${word}について、以下の見出しで300字程度で解説するマークダウンファイルを作成してください。
+              \`\`\`md
+              ## [タイトル]
+              - 概要
+              - 具体例
+              - 関連語句
+              \`\`\`
+              `;
+            const response = await axios.post(
+                `${API_URL}chat/completions`,
+                {
+                    model: MODEL,
+                    messages: [
+                        {
+                            role: "user",
+                            content: prompt,
+                        },
+                    ],
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${API_KEY}`,
+                    },
+                }
+            );
+
+            const newContent = {
+                content: response.data.choices[0].message.content,
+                check: false,
+            };
+
+            setContents((prevContents) => [...prevContents, newContent]);
+            setLoading(false);
+        } catch (error) {
+            console.error("Error get explain: ", error);
+        }
     };
 
     //--------------------------------------------
-
     const repoBApiUrl =
         "https://api.github.com/repos/{owner}/{repo}/contents/{path}";
     const owner = "Yamayamaaya";
@@ -108,14 +199,20 @@ function App() {
     };
 
     // MDファイルγの書き換えとプッシュ
-    const updateMDFile = async (requestUrl, content, headers, mdFile) => {
+    const updateMDFile = async (
+        requestUrl,
+        content,
+        explains,
+        headers,
+        mdFile
+    ) => {
         console.log("updateMDFile");
         console.log(content);
         let existingContent;
         let sha;
 
         if (content) {
-            existingContent = atob(content.content); // Base64デコード
+            existingContent = decodeURIComponent(escape(atob(content.content))); // Base64デコード
             sha = content.sha;
             console.log("existingContent", existingContent);
         } else {
@@ -126,16 +223,17 @@ function App() {
         // 条件に応じてMDファイルγの内容を書き換える（ここに必要な処理を追加）
         let newContent;
         if (existingContent) {
-            newContent = existingContent.toUpperCase(); // 例: 内容を大文字に変換
+            // 既存の内容に追記
+            newContent = existingContent + "\n" + explains; // 新しいファイルの内容を指定
         } else {
-            newContent = "New file content"; // 新しいファイルの内容を指定
+            newContent = explains; // 新しいファイルの内容を指定
         }
         console.log("newContent", newContent);
 
         // 書き換えたMDファイルγをリポジトリBにプッシュ
         const data = {
             message: `Update ${mdFile}`,
-            content: btoa(newContent), // Base64エンコード
+            content: btoa(unescape(encodeURIComponent(newContent))), // Base64エンコード
             sha: sha,
         };
 
@@ -169,37 +267,35 @@ function App() {
     };
 
     // メインの処理フロー
-    const main = async () => {
+    const main = async (explains) => {
         try {
             const mdFile = await generateNewFileName();
             const requestUrl = getRequestUrl(mdFile);
             const token = getToken();
             const headers = getHeaders(token);
             const content = await fetchMDFile(requestUrl, headers);
-            await updateMDFile(requestUrl, content, headers, mdFile);
+            await updateMDFile(requestUrl, content, explains, headers, mdFile);
         } catch (error) {
             console.error("Error:", error);
         }
     };
 
-    //--------------------------------------------
     return (
-        <div className="flex flex-col min-h-screen">
-            <header className="fixed top-0 w-full bg-gray-800 text-white py-4 px-8">
-                <h1 className="text-2xl">タイトル</h1>
+        <div>
+            <header>
+                <h1>タイトル</h1>
             </header>
 
-            <main className="flex-1 pt-10">
-                <div id="input-fields" className="py-8 px-8">
+            <main>
+                <div id="input-fields">
                     {inputSets.map((set, index) => (
                         <div key={index} className="input-set mb-4">
                             <input
                                 type="text"
-                                name="term"
-                                value={set.term}
+                                name="word"
+                                value={set.word}
                                 placeholder="用語入力欄"
                                 onChange={(e) => handleInputChange(index, e)}
-                                className="border border-gray-300 rounded py-2 px-4 mr-2 focus:outline-none focus:border-blue-500"
                             />
                             <input
                                 type="text"
@@ -207,19 +303,14 @@ function App() {
                                 value={set.genre}
                                 placeholder="ジャンル入力欄"
                                 onChange={(e) => handleInputChange(index, e)}
-                                className="border border-gray-300 rounded py-2 px-4 mr-2 focus:outline-none focus:border-blue-500"
                             />
                             {index === inputSets.length - 1 && (
                                 <>
-                                    <button
-                                        onClick={handleAddSet}
-                                        className="bg-blue-500 text-white py-2 px-4 rounded mr-2"
-                                    >
+                                    <button onClick={handleAddSet}>
                                         セット追加
                                     </button>
                                     <button
                                         onClick={() => handleRemoveSet(index)}
-                                        className="bg-red-500 text-white py-2 px-4 rounded"
                                     >
                                         セット削除
                                     </button>
@@ -227,17 +318,34 @@ function App() {
                             )}
                         </div>
                     ))}
+                    <button onClick={handleSubmitToOpenAi}>説明</button>
+                    {loading && <p>ローディング中...</p>}
                 </div>
-
-                <button
-                    onClick={handleSubmit}
-                    className="bg-green-500 text-white py-2 px-4 rounded ml-8 mb-8"
-                >
-                    送信
-                </button>
+                <div id="output-fields">
+                    {
+                        // console.log(contents)
+                        contents.map((content, index) => (
+                            <div key={index} className="output-set">
+                                <textarea
+                                    value={content.content}
+                                    placeholder="文章出力欄"
+                                    onChange={(e) =>
+                                        handleContentChange(index, e)
+                                    }
+                                />
+                                <input
+                                    type="checkbox"
+                                    checked={content.check}
+                                    onChange={() => handleCheckChange(index)}
+                                />
+                            </div>
+                        ))
+                    }
+                    <button onClick={handleSubmitToZenn}>zennに保存</button>
+                </div>
             </main>
 
-            <footer className="fixed bottom-0 w-full bg-gray-800 text-white py-4 px-8">
+            <footer>
                 <p>2023 ©️Yamayamaaya</p>
             </footer>
         </div>
